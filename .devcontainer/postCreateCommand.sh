c985a783c10b22315401e654c308ce8cc2bbfdf7
Alex Holms <superlinkx@gmail.com>
2023-08-05 23:03:11 +0000

    fix: postCreateCommand

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,3 +3,4 @@ go install github.com/sqlc-dev/sqlc/cmd/sqlc@v1.20.0
 go install github.com/rubenv/sql-migrate/...@v1.5.2
 go install github.com/cosmtrek/air@v1.44.0
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
+go mod init
