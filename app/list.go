e581f84bcd51d2c8a4c36df7e0e7c85439f5655e
Alice <nightfire4@gmail.com>
2023-09-09 21:36:49 +0000

    chore: added tests for ListItem chore: fixed some type conversions

--- app/list.go
+++ app/list.go
@@ -38,10 +38,7 @@ func (a Application) AllLists(ctx context.Context, limit int64) ([]List, error) 
 		return lists, fmt.Errorf("application failed enumerate lists: %w", err)
 	} else {
 		for _, result := range results {
-			lists = append(lists, List{
-				ID:   result.ID,
-				Name: result.Name,
-			})
+			lists = append(lists, List(result))
 		}
 
 		return lists, nil
@@ -52,7 +49,7 @@ func (a Application) GetList(ctx context.Context, id int64) (List, error) {
 	if result, err := a.services.List.GetList(ctx, id); err != nil {
 		return List{}, fmt.Errorf("application failed to get list with id %d: %w", id, err)
 	} else {
-		return List{ID: result.ID, Name: result.Name}, nil
+		return List(result), nil
 	}
 }
 
@@ -60,7 +57,7 @@ func (a Application) CreateList(ctx context.Context, name string) (List, error) 
 	if result, err := a.services.List.CreateList(ctx, name); err != nil {
 		return List{}, fmt.Errorf("application failed to create list: %w", err)
 	} else {
-		return List{ID: result.ID, Name: result.Name}, nil
+		return List(result), nil
 	}
 }
 
@@ -68,7 +65,7 @@ func (a Application) UpdateList(ctx context.Context, id int64, name string) (Lis
 	if result, err := a.services.List.UpdateList(ctx, id, name); err != nil {
 		return List{}, fmt.Errorf("application failed to update list: %w", err)
 	} else {
-		return List{ID: result.ID, Name: result.Name}, nil
+		return List(result), nil
 	}
 }
 
