32e5431bd70c83154843279d08861f683542545e
Alyx Holms <superlinkx@gmail.com>
2023-12-20 23:49:31 +0000

    chore: name cleanup

--- data/postgresql/adapter/adapter.go
+++ data/postgresql/adapter/adapter.go
@@ -31,15 +31,15 @@ import (
 	"github.com/superlinkx/HomeList/data/postgresql/sqlc"
 )
 
-type SqliteAdapter struct {
+type PostgresAdapter struct {
 	queries *sqlc.Queries
 }
 
-func NewSqliteAdapter(db *sql.DB) adapter.Adapter {
-	return SqliteAdapter{queries: sqlc.New(db)}
+func NewPostgresAdapter(db *sql.DB) adapter.Adapter {
+	return PostgresAdapter{queries: sqlc.New(db)}
 }
 
-func (s SqliteAdapter) AllLists(ctx context.Context, limit int32) ([]adapter.List, error) {
+func (s PostgresAdapter) AllLists(ctx context.Context, limit int32) ([]adapter.List, error) {
 	if lists, err := s.queries.AllLists(ctx, limit); err != nil {
 		return nil, fmt.Errorf("failed to get lists: %w", err)
 	} else {
@@ -51,7 +51,7 @@ func (s SqliteAdapter) AllLists(ctx context.Context, limit int32) ([]adapter.Lis
 	}
 }
 
-func (s SqliteAdapter) GetList(ctx context.Context, id int64) (adapter.List, error) {
+func (s PostgresAdapter) GetList(ctx context.Context, id int64) (adapter.List, error) {
 	if list, err := s.queries.GetList(ctx, id); err != nil {
 		return adapter.List{}, fmt.Errorf("failed to get list: %w", err)
 	} else {
@@ -59,7 +59,7 @@ func (s SqliteAdapter) GetList(ctx context.Context, id int64) (adapter.List, err
 	}
 }
 
-func (s SqliteAdapter) RenameList(ctx context.Context, id int64, name string) (adapter.List, error) {
+func (s PostgresAdapter) RenameList(ctx context.Context, id int64, name string) (adapter.List, error) {
 	if list, err := s.queries.RenameList(ctx, sqlc.RenameListParams{ID: id, Name: name}); err != nil {
 		return adapter.List{}, fmt.Errorf("failed to rename list: %w", err)
 	} else {
@@ -67,7 +67,7 @@ func (s SqliteAdapter) RenameList(ctx context.Context, id int64, name string) (a
 	}
 }
 
-func (s SqliteAdapter) CreateList(ctx context.Context, name string) (adapter.List, error) {
+func (s PostgresAdapter) CreateList(ctx context.Context, name string) (adapter.List, error) {
 	if list, err := s.queries.CreateList(ctx, name); err != nil {
 		return adapter.List{}, fmt.Errorf("failed to create list: %w", err)
 	} else {
@@ -75,11 +75,11 @@ func (s SqliteAdapter) CreateList(ctx context.Context, name string) (adapter.Lis
 	}
 }
 
-func (s SqliteAdapter) DeleteList(ctx context.Context, id int64) error {
+func (s PostgresAdapter) DeleteList(ctx context.Context, id int64) error {
 	return s.queries.DeleteList(ctx, id)
 }
 
-func (s SqliteAdapter) AllItemsFromList(ctx context.Context, listID int64, limit int32) ([]adapter.ListItem, error) {
+func (s PostgresAdapter) AllItemsFromList(ctx context.Context, listID int64, limit int32) ([]adapter.ListItem, error) {
 	if items, err := s.queries.AllItemsFromList(ctx, sqlc.AllItemsFromListParams{ListID: listID, Limit: limit}); err != nil {
 		return nil, fmt.Errorf("failed to get items from list: %w", err)
 	} else {
@@ -91,7 +91,7 @@ func (s SqliteAdapter) AllItemsFromList(ctx context.Context, listID int64, limit
 	}
 }
 
-func (s SqliteAdapter) CreateListItem(ctx context.Context, listID int64, content string, sort int64) (adapter.ListItem, error) {
+func (s PostgresAdapter) CreateListItem(ctx context.Context, listID int64, content string, sort int64) (adapter.ListItem, error) {
 	if item, err := s.queries.CreateListItem(ctx, sqlc.CreateListItemParams{ListID: listID, Content: content, Sort: sort}); err != nil {
 		return adapter.ListItem{}, fmt.Errorf("failed to create list item: %w", err)
 	} else {
@@ -99,11 +99,11 @@ func (s SqliteAdapter) CreateListItem(ctx context.Context, listID int64, content
 	}
 }
 
-func (s SqliteAdapter) DeleteListItem(ctx context.Context, id int64) error {
+func (s PostgresAdapter) DeleteListItem(ctx context.Context, id int64) error {
 	return s.queries.DeleteListItem(ctx, id)
 }
 
-func (s SqliteAdapter) GetListItem(ctx context.Context, id int64) (adapter.ListItem, error) {
+func (s PostgresAdapter) GetListItem(ctx context.Context, id int64) (adapter.ListItem, error) {
 	if item, err := s.queries.GetListItem(ctx, id); err != nil {
 		return adapter.ListItem{}, fmt.Errorf("failed to get list item: %w", err)
 	} else {
@@ -111,7 +111,7 @@ func (s SqliteAdapter) GetListItem(ctx context.Context, id int64) (adapter.ListI
 	}
 }
 
-func (s SqliteAdapter) UpdateListItemChecked(ctx context.Context, id int64, checked bool) (adapter.ListItem, error) {
+func (s PostgresAdapter) UpdateListItemChecked(ctx context.Context, id int64, checked bool) (adapter.ListItem, error) {
 	if item, err := s.queries.UpdateListItemChecked(ctx, sqlc.UpdateListItemCheckedParams{ID: id, Checked: checked}); err != nil {
 		return adapter.ListItem{}, fmt.Errorf("failed to update list item checked: %w", err)
 	} else {
@@ -119,7 +119,7 @@ func (s SqliteAdapter) UpdateListItemChecked(ctx context.Context, id int64, chec
 	}
 }
 
-func (s SqliteAdapter) UpdateListItemSort(ctx context.Context, id int64, sort int64) (adapter.ListItem, error) {
+func (s PostgresAdapter) UpdateListItemSort(ctx context.Context, id int64, sort int64) (adapter.ListItem, error) {
 	if item, err := s.queries.UpdateListItemSort(ctx, sqlc.UpdateListItemSortParams{ID: id, Sort: sort}); err != nil {
 		return adapter.ListItem{}, fmt.Errorf("failed to update list item sort: %w", err)
 	} else {
@@ -127,7 +127,7 @@ func (s SqliteAdapter) UpdateListItemSort(ctx context.Context, id int64, sort in
 	}
 }
 
-func (s SqliteAdapter) UpdateListItemText(ctx context.Context, id int64, content string) (adapter.ListItem, error) {
+func (s PostgresAdapter) UpdateListItemText(ctx context.Context, id int64, content string) (adapter.ListItem, error) {
 	if item, err := s.queries.UpdateListItemText(ctx, sqlc.UpdateListItemTextParams{ID: id, Content: content}); err != nil {
 		return adapter.ListItem{}, fmt.Errorf("failed to update list item text: %w", err)
 	} else {
